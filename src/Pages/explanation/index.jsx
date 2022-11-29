a6ebe371971aa22e1a1b46874329fc4d6b7878f0
Alexandre <alepranskunas@hotmail.com>
2022-11-29 20:08:35 -0300

    Estrutura da pagina explanation

---
+++ src/Pages/explanation/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import DefaultButton from "../../Compponents/Common/DefaultButton";
+
+
+export default function AppExpalnation() {
+
+    function handleSetShowHome(){
+        console.log('funcionou')
+    }
+
+    return(
+        <View style={estilo.container}>
+            <ScrollView>
+                <View style={{alignItems: 'center'}}>
+                    <Text style={estilo.title}>
+                        Antes,   deixa {'\n'}eu te explicar...
+                    </Text>
+                    <Text style={estilo.descriptionCta}>
+                        Pronto(a) para subir de nivel na vida?
+                    </Text>
+                    <Text style={estilo.description}>
+                        Na proxima tela voce vai poder escolher {'\n'} seus 4 hábitos de forma individual.
+                    </Text>
+                    <DefaultButton
+                        buttonText={'Continuar'}
+                        handlePress={handleSetShowHome}
+                        width={250}
+                        height={50}
+                    />
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const estilo = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(21,21,21,0.98)',
+    },
+
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        textAlign: 'center',
+        marginVertical: 40
+    },
+
+    descriptionCta: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginTop: 20,
+        marginBottom: 10
+    },
+    
+    description: {
+        color: '#fff',
+        textAlign: 'center',
+        marginBottom: 30
+    }
+})
