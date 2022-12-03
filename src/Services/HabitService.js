102058ba73fb07c4b3a353507fca2c144e380c82
Alexandre <alepranskunas@hotmail.com>
2022-12-03 11:21:25 -0300

    Criando serviço de criação do habito

--- src/Services/HabitService.js
+++ src/Services/HabitService.js
@@ -9,3 +9,41 @@ db.transaction((tx) => {
         }
     )
 })
+
+const createHabit = (obj) => {
+
+    return new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            tx.executeSql(
+                "INSERT INTO habits (habitArea, habitName, habitFrequency, habitHasNotification, habitNotificationFrequency, habitNotificationTime, lastCheck, daysWithOutChecks, progressBar, habitIsCheked, habitCheks) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                [
+                    obj.habitArea,
+                    obj.habitName,
+                    obj.habitFrequency, 
+                    obj.habitHasNotification, 
+                    obj.habitNotificationFrequency, 
+                    obj.habitNotificationTime, 
+                    obj.lastCheck,
+                    obj.daysWithOutChecks, 
+                    obj.progressBar, 
+                    obj.habitIsCheked, 
+                    obj.habitChek
+                ],
+                (_,{rowsAffected, insertId})=>{
+                    if(rowsAffected > 0 ){
+                        resolve(insertId)
+                    }else {
+                        reject("erro: "+ JSON.stringify(obj))
+                    }
+                },
+                (_,error)=>{
+                    reject(error)
+                }
+            )
+        })
+    })
+}
+
+export default {
+    createHabit
+}
